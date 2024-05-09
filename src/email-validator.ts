19b624723c49a52a79b0648e932e0a49fc06b9a5
bjarneo <bjarne.oeverli@gmail.com>
2024-05-09 22:38:40 +0200

    chore: add another basic check

--- src/email-validator.ts
+++ src/email-validator.ts
@@ -2,7 +2,9 @@ export const emailRegex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 function hasBasicEmailStructure(email: string): boolean {
-    return email.includes('@') && email.split('@')[1].length > 0;
+    return (
+        email.includes('@') && email.split('@')[1].length > 0 && email.split('@')[1].includes('.')
+    );
 }
 
 export function validateEmail(email: string): boolean {
