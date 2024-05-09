9e80233de911fe1f7b878ceb2f1318bb6e0069ec
bjarneo <bjarne.oeverli@gmail.com>
2024-05-09 21:41:44 +0200

    featinitial code

---
+++ README.md
@@ -0,0 +1,31 @@
+# Email Validator As A Service [EVaaS]
+
+This is based on the RFC 5322 Official Standard. This service is made for fun based on this tweet using the perl RFC5322 email regex: https://twitter.com/mSykeCodes/status/1788446683921285238
+
+## Usage
+
+```bash
+# Using CURL
+curl -X POST -H "Content-Type: application/json" -d '{"email":"what@what.com"}' https://evaas.dothash.workers.dev
+{"message":"RFC 5322 - Valid email address","status":1}
+
+# Using HTTPie
+http -b https://evaas.dothash.workers.dev email=what@wat.comm
+{
+    "message": "RFC 5322 - Valid email address",
+    "status": 1
+}
+```
+
+## Development
+
+```
+npm install
+npm run dev
+```
+
+## Deployment to Cloudflare Workers
+
+```
+npm run deploy
+```
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
+import { validator } from 'hono/validator';
+
+import { validateEmail, emailRegex } from './email-regex';
+
+const app = new Hono();
+
+const RFC5322 = 'RFC 5322';
+
+enum EmailStatus {
+    VALID = 1,
+    INVALID = 2,
+}
+
+app.use('/*', cors());
+app.use(logger());
+
+app.get('/', (c) => {
+    return c.text(`${RFC5322}- ${emailRegex.source}`);
+});
+
+app.post(
+    '/',
+    validator('json', async (_, c) => {
+        const email = (await c.req.json()).email;
+        if (!validateEmail(email)) {
+            return c.json(
+                { message: `${RFC5322} - Invalid email address`, status: EmailStatus.INVALID },
+                { status: 400 }
+            );
+        }
+    }),
+    (c) => {
+        return c.json(
+            { message: `${RFC5322} - Valid email address`, status: EmailStatus.VALID },
+            { status: 200 }
+        );
+    }
+);
+
+export default app;
