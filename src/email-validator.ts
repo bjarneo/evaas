64eb6b731ddb8f8f7f0b2b19bac87a5a330c8545
bjarneo <bjarne.oeverli@gmail.com>
2024-05-09 22:20:34 +0200

    chore: check for basic structure before the rfc regex

--- src/email-validator.ts
+++ src/email-validator.ts
@@ -1,6 +1,10 @@
 export const emailRegex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+function hasBasicEmailStructure(email: string): boolean {
+    return email.includes('@') && email.split('@')[1].length > 0;
+}
+
 export function validateEmail(email: string): boolean {
-    return emailRegex.test(email);
+    return hasBasicEmailStructure(email) && emailRegex.test(email);
 }
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
 import { validator } from 'hono/validator';
 
-import { validateEmail, emailRegex } from './email-regex';
+import { validateEmail, emailRegex } from './email-validator';
 
 const app = new Hono();
 
